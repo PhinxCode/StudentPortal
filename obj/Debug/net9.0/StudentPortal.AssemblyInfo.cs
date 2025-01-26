473d36770a4da48ac774e1a2436394a332b9784e
Null <phinxcode@gmail.com>
2025-01-26 11:18:14 -0400

    updateList

--- obj/Debug/net9.0/StudentPortal.AssemblyInfo.cs
+++ obj/Debug/net9.0/StudentPortal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentPortal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64a5bf855609789627c07b21aae4c938b01039a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3eaec255a92d4502971525af79383981302e39d6")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentPortal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentPortal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
